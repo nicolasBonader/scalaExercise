d7f45d7ced9e8d21aafaf921d53edadc4e6c5248
Nicolás Bonader <neb0113@famaf.unc.edu.ar>
2018-10-29 01:38:44 -0300

    some unit tests for Rental class

--- src/main/scala/Rental.scala
+++ src/main/scala/Rental.scala
@@ -1,4 +1,4 @@
-trait RentalType
+sealed trait RentalType
 case class Hourly() extends RentalType
 case class Daily() extends RentalType
 case class Weekly() extends RentalType
@@ -15,24 +15,26 @@ class Rental(val startValue: Long, val endValue: Option[Long] = None, val `type`
   private val milisInADay = milisInAnHour * 24
   private val milisInAWeek = milisInADay * 7
 
+  def this(startValue: Long, endValue: Long, `type`: RentalType) = this(startValue, Some(endValue), `type`)
+
+  def this(startValue: Long, endValue: Long) = this(startValue, Some(endValue))
+
+  def this(startValue: Long, `type`: RentalType) = this(startValue, None, `type`)
+
+  def this(startValue: Long) = this(startValue, Hourly())
+
   def getCost(): Option[Int] = {
     endValue map { someEndValue =>
       val timeDiff = someEndValue - startValue
       `type` match {
         case Hourly() => costPerHour * getHours(timeDiff)
-        case Daily() => costADay + costPerHour * getHours(Math.max(timeDiff - milisInADay, 0))
-        case Weekly() => costAWeek + costPerHour * getHours(Math.max(timeDiff - milisInAWeek, 0))
+        case Daily() => costADay + costPerHour * getHours(timeDiff - milisInADay)
+        case Weekly() => costAWeek + costPerHour * getHours(timeDiff - milisInAWeek)
       }
     }
   }
 
   private def getHours(deltaMilis: Long): Int = {
-    Math.ceil(deltaMilis / milisInAnHour).toInt
+    Math.ceil(Math.max(deltaMilis, 0) / milisInAnHour.toDouble).toInt
   }
-}
-
-object App extends App {
-  val r = new Rental(0, Some(0))
-  println("Hola")
-  println(r.startValue)
 }
